af6c24f6dc8f9b5006e201b5cc02bcc2d28b764e
Paulo Ferreira <paulovictorfcosta@gmail.com>
2022-09-22 15:31:37 -0300

    Feat: Req 2.3 Classes Halfling e Orc

---
+++ src/Races/Orc.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Orc extends Race {
+  private _maxLifePoints: number;
+  private static createdOrcs = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 60;
+    Orc.createdOrcs += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  public static createdRacesInstances(): number {
+    return this.createdOrcs;
+  }
+}
+
+export default Orc;
